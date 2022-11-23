2ac1ba156aca14441b2c387cb697fc8cc151fb41
migcruzz <101120553+migcruzz@users.noreply.github.com>
2022-11-23 12:18:48 +0000

    Complete Homepage

--- app/src/main/java/com/example/login_project/Login/LoginActivity.kt
+++ app/src/main/java/com/example/login_project/Login/LoginActivity.kt
@@ -24,6 +24,7 @@ class LoginActivity : AppCompatActivity() {
 
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
